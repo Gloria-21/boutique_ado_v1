4599a7d69aae3d4d317008cbc3bc5d0f81958449
Gloria-21 <gloriahn_82@hotmail.com>
2022-04-20 22:58:20 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KqlEyGQ9tfLIql4vUUDUJ2yKDiWDHQ358jRFiHXP4yLdBr2QIlOWtgOouq33KqwpnPD9wF6k5g1MQmAzqWhOGzc00md5S5KLv',
+        'client_secret': 'test client secret', 
     }
 
     return render(request, template, context)
